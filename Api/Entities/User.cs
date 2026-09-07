11e2875746603a18ee649d463df29e7ed50ca735
Tyler Boutwell <tylerboutwell1@gmail.com>
2026-09-07 15:24:48 -0500

    create user entity

---
+++ Api/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Entities
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
