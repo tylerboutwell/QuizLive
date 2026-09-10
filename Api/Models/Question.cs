75386e6e761a65c5a74960797a1c343f3b841e26
Tyler Boutwell <tylerboutwell1@gmail.com>
2026-09-10 05:53:30 -0500

    add question model fields

--- Api/Models/Question.cs
+++ Api/Models/Question.cs
@@ -2,5 +2,20 @@
 {
     public class Question
     {
+        public int Id { get; set; }
+
+        public int QuizId { get; set; }
+
+        public string? Text { get; set; }
+
+        public string? OptionA { get; set; }
+
+        public string? OptionB { get; set; }
+
+        public string? OptionC { get; set; }
+
+        public string? OptionD { get; set; }
+
+        public string? CorrectOption { get; set; }
     }
 }
