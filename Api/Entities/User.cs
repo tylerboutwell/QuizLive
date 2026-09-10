6d1ecdc758a7071a256ae9cef8e87687c99e0c4c
Tyler Boutwell <tylerboutwell1@gmail.com>
2026-09-09 19:11:11 -0500

    remove user model - implement authentication/authorization later

--- Api/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Api.Entities
-{
-    public class User
-    {
-        public string Username { get; set; } = string.Empty;
-
-        public string PasswordHash { get; set; } = string.Empty;
-    }
-}
--- Api/Models/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Api.Models
-{
-    public class UserDto
-    {
-        public string Username { get; set; } = string.Empty;
-
-        public string Password { get; set; } = string.Empty;
-    }
-}
