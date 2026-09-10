fbc0851599d0a6d72ef34459600a112191987282
Tyler Boutwell <tylerboutwell1@gmail.com>
2026-09-10 15:39:06 -0500

    create quizlive db context

---
+++ Api/Endpoints/Question.cs
@@ -0,0 +1,20 @@
+﻿namespace Api.Endpoints
+{
+    public class Question
+    {
+        public static void Map(WebApplication app)
+        {
+            app.MapGet("/", async context =>
+            {
+                // Get all questions
+                await context.Response.WriteAsJsonAsync(new { Message = "All todo items" });
+            });
+
+            app.MapGet("/{id}", async context =>
+            {
+                // Get one question
+                await context.Response.WriteAsJsonAsync(new { Message = "One todo item" });
+            });
+        }
+    }
+}
---
+++ Api/QuizLiveDb.cs
@@ -0,0 +1,13 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+class QuizLiveDb : DbContext
+{
+    public QuizLiveDb(DbContextOptions<QuizLiveDb> options)
+        : base(options) { }
+
+    public DbSet<Question> Questions => Set<Question>();
+    public DbSet<Player> Players => Set<Player>();
+    public DbSet<Quiz> Quizzes => Set<Quiz>();
+    public DbSet<Game> Games => Set<Game>();
+}
