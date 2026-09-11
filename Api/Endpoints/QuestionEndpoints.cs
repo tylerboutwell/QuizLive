5db684c4ae642327202503f00777e944c266e04f
Tyler Boutwell <tylerboutwell1@gmail.com>
2026-09-11 06:43:09 -0500

    change questionendpoints results to TypedResults

--- Api/Endpoints/QuestionEndpoints.cs
+++ Api/Endpoints/QuestionEndpoints.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Api.Endpoints;
 
@@ -8,30 +9,31 @@ public class QuestionEndpoints
     public static void Map(WebApplication app)
     {
         var questions = app.MapGroup("/questions");
-        questions.MapGet("/", async (QuizLiveDb db) =>
+        questions.MapGet("/", async Task<Ok<List<Question>>> (QuizLiveDb db) =>
         {
             var questions = await db.Questions.ToListAsync();
 
-            return Results.Ok(questions);
+            return TypedResults.Ok(questions);
         });
-        questions.MapGet("/{id}", async (int id, QuizLiveDb db) =>
+
+        questions.MapGet("/{id}", async Task<Results<Ok<Question>, NotFound>> (int id, QuizLiveDb db) =>
             await db.Questions.FindAsync(id)
                 is Question question
-                ? Results.Ok(question)
-                : Results.NotFound());
+                ? TypedResults.Ok(question)
+                : TypedResults.NotFound());
 
 
-        questions.MapPost("/", async (Question question,QuizLiveDb db) =>
+        questions.MapPost("/", async Task<Created<Question>> (Question question,QuizLiveDb db) =>
         {
             db.Add(question);
             await db.SaveChangesAsync();
-            return Results.Created($"/{question.Id}", question);
+            return TypedResults.Created($"/{question.Id}", question);
         });
 
-        questions.MapPut("/{id}", async (int id, Question inputQuestion, QuizLiveDb db) =>
+        questions.MapPut("/{id}", async Task<Results<NotFound, NoContent>> (int id, Question inputQuestion, QuizLiveDb db) =>
         {
             var question = await db.Questions.FindAsync(id);
-            if (question is null) return Results.NotFound();
+            if (question is null) return TypedResults.NotFound();
             question.Text = inputQuestion.Text;
             question.OptionA = inputQuestion.OptionA;
             question.OptionB = inputQuestion.OptionB;
@@ -40,18 +42,18 @@ public class QuestionEndpoints
             question.CorrectOption = inputQuestion.CorrectOption;
 
             await db.SaveChangesAsync();
-            return Results.NoContent();
+            return TypedResults.NoContent();
         });
 
-        questions.MapDelete("/{id}", async (int id, QuizLiveDb db) =>
+        questions.MapDelete("/{id}", async Task<Results<NoContent, NotFound>> (int id, QuizLiveDb db) =>
         {
             if (await db.Questions.FindAsync(id) is Question question)
             {
                 db.Questions.Remove(question);
                 await db.SaveChangesAsync();
-                return Results.NoContent();
+                return TypedResults.NoContent();
             }
-            return Results.NotFound();
+            return TypedResults.NotFound();
         });
     }
 }
