34ec18cd6a00b51b8c7a5ccf4eb286d8ca1bb82f
Tyler Boutwell <tylerboutwell1@gmail.com>
2026-09-22 06:58:40 -0500

    create function in next.js for creating game

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,25 @@
+'use client';
+import { API_URL } from "@/lib/api";
+
 export default function Home() {
+    const createGame = async () => {
+        const response = await fetch(`${API_URL}/games`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to create game");
+        }
+
+        const game = await response.json();
+
+        console.log(game);
+    }
+
+
   return (
     <main className="min-h-screen flex items-center justify-center">
       <div className="text-center space-y-6">
@@ -9,13 +30,13 @@ export default function Home() {
         </p>
 
         <div className="flex gap-4 justify-center">
-          <button className="px-6 py-3 rounded-lg bg-black text-white">
-            Create Game
-          </button>
+            <button onClick={createGame} className="px-6 py-3 rounded-lg bg-black text-white">
+                Create Game
+            </button>
 
-          <button className="px-6 py-3 rounded-lg border border-gray-300">
+            <button className="px-6 py-3 rounded-lg border border-gray-300">
             Join Game
-          </button>
+            </button>
         </div>
       </div>
     </main>
